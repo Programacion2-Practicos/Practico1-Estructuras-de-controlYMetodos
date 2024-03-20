57bfdf3283def09757f3f1075042c218a9394b9b
Elizabeth Izquierdo <73123352+eliizquierdo@users.noreply.github.com>
2024-03-20 20:55:58 -0300

    Update Programa1.java

--- src/paquete/Programa1.java
+++ src/paquete/Programa1.java
@@ -13,29 +13,11 @@ public class Programa1 {
 	
 	
 	//Cargar datos 
-	
 	public void cargar() {
-		System.out.println("Ingrese su apellido: ");
-		apellido = entrada.next();
-		
-		System.out.println("Ingrese su edad: ");
-		edad = entrada.nextInt();
-		
-		System.out.println("Ingrese su altura: ");
-		altura = entrada.nextDouble();
-		
-		System.out.println("Ingrese su cedula: ");
-		cedula = entrada.nextInt();
-	}
+	//complete aquí---------------------------
 	
 	//Imprimir datos
-	
 	public void imprimir() {
-		
-		System.out.println("Su Apellido es: " + apellido);
-		System.out.println("Su Edad es: " + edad);
-		System.out.println("Su Altura es: " + altura);
-		System.out.println("Su Cedula es: " + cedula);
-		
+	//complete aquí------------------------
 	}
 }

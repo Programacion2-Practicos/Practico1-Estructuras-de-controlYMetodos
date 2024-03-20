6485cd21d6ed5e46c4cc3b173f3f5df19e90e3f9
Elizabeth Izquierdo <73123352+eliizquierdo@users.noreply.github.com>
2024-03-20 20:47:04 -0300

    Add files via upload

--- src/paquete/Main.java
+++ src/paquete/Main.java
@@ -19,7 +19,26 @@ public class Main {
 		System.out.println("----------EJERCICIO 4----------");
 		Programa4 ejer4 = new Programa4();
 		ejer4.inicio4();
+		
+		System.out.println("----------PARTE1a----------");
+		Parte1a ej1 = new Parte1a();
+		ej1.iniciarParte1a();
+		
+		System.out.println("----------PARTE1b----------");
+		Parte1b ej2 = new Parte1b();
+		ej2.iniciarParte1b();
+		
+		System.out.println("----------PARTE1c----------");
+		Parte1c ej3 = new Parte1c();
+		ej3.iniciarParte1c();
+		
+		System.out.println("----------PARTE1d----------");
+		Parte1d ej4 = new Parte1d();
+		ej4.iniciarParte1d();
+
 
 	}
+	
+	
 
 }

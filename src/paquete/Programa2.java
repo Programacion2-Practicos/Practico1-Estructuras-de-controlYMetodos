a39dafdb687296f317d4fa2111a51599eb805918
Elizabeth Izquierdo <73123352+eliizquierdo@users.noreply.github.com>
2024-03-20 20:57:43 -0300

    Update Programa2.java

--- src/paquete/Programa2.java
+++ src/paquete/Programa2.java
@@ -11,46 +11,23 @@ public class Programa2 {
 		int num;
 		String resul;
 		
-		do{
-			num=pedirNumero();
-			resul=compararNumeros(num, valor);
-			System.out.println(resul);
-			
-		}while(!(resul.equals("igual")));
-		System.out.println("Felicitaciones acertaste!!");
+		//complete aquí-----------------------------
 	}
 	
 	//devuelve un numero aleatorio entre 1 y 100
 	public int generarAleatorio(){
-		Random aleatorio=new Random();
-		
-		int valor= 1 + aleatorio.nextInt(100);
-		return valor;
+		//complete aquí-----------------------------
 	}
 	
 	//pide un nro por pantalla
 	 public int pedirNumero(){
-		 
-		 Scanner entrada=new Scanner(System.in);
-		 
-		 System.out.println("Ingrese numero: ");
-		 int numero=entrada.nextInt();
-		 return numero;
+		 //complete aquí-----------------------------
 		 
 	 }
 	
 	 //compara dos enteros a y b. Devuelve un String indicando si es menor, mayor o igual
 	 public String compararNumeros(int a, int b){
-		 String esMenor;
-		 if (a < b)
-				esMenor="Demasiado chico";
-		 else
-				if (a > b)
-					esMenor="Demasiado grande";
-				else
-					esMenor="igual";
-	 
-		 return esMenor;
+		 //complete aquí-----------------------------
 	 }
 	
 }

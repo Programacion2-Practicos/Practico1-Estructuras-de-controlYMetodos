41aff278ee3b7fc2fb5ca9165a570c5adb5ac933
Elizabeth Izquierdo <73123352+eliizquierdo@users.noreply.github.com>
2024-03-20 20:00:58 -0300

    Create Main.java

---
+++ src/paquete/Main.java
@@ -0,0 +1,25 @@
+package paquete;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("----------EJERCICIO 1----------");
+		Programa1 ejer1 = new Programa1();
+		ejer1.cargar();
+		ejer1.imprimir();
+		
+		System.out.println("----------EJERCICIO 2----------");
+		Programa2 ejer2 = new Programa2();
+		ejer2.juego();
+		
+		System.out.println("----------EJERCICIO 3----------");
+		Programa3 ejer3 = new Programa3();
+		ejer3.iniciar();
+		
+		System.out.println("----------EJERCICIO 4----------");
+		Programa4 ejer4 = new Programa4();
+		ejer4.inicio4();
+
+	}
+
+}
